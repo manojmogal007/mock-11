2a071f49b3c046ee9510d34bb106189262cabb50
manojmogal007 <manojmogal07@gmail.com>
2023-02-25 14:44:07 +0530

    add

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const {userRouter}=require('./routes/user.route')
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use('/user',userRouter)
 
 
